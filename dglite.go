478014f035b79d9367acd0f22a0bdad1e7945633
Joschka Tillmanns <joschka@tillmanns.me>
2019-12-29 22:16:36 +0100

    add a public API and a more comprehensive test

---
+++ dglite.go
@@ -0,0 +1,91 @@
+package dglite
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"mooncamp.com/dgx/gql"
+)
+
+type RDF struct {
+	Subject         uint64
+	Object          interface{}
+	Predicate, Type string
+}
+
+func (rdf RDF) String() string {
+	return fmt.Sprintf("<%d> <%s> <%v>:%s", rdf.Subject, rdf.Predicate, rdf.Object, rdf.Type)
+}
+
+type RDFs []RDF
+
+func (s RDFs) Len() int           { return len(s) }
+func (s RDFs) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s RDFs) Less(i, j int) bool { return s[i].Subject < s[j].Subject }
+
+type Schema struct {
+	Predicate string
+	Many      bool
+	Type      string
+}
+
+type DGLite interface {
+	Write(interface{}) ([]uint64, error)
+	Read([]gql.GraphQuery, interface{}) error
+}
+
+type dglite struct {
+	reader *reader
+	writer *writer
+
+	rdfs []RDF
+}
+
+func New(schema []Schema) DGLite {
+	return &dglite{
+		reader: &reader{schemas: schema},
+		writer: newWriter(),
+	}
+}
+
+func (dgl *dglite) Write(in interface{}) ([]uint64, error) {
+	js, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	var ns []map[string]interface{}
+	if err := json.Unmarshal(js, &ns); err != nil {
+		return nil, err
+	}
+
+	uids := []uint64{}
+	for _, n := range ns {
+		rdfs, uid := dgl.writer.rdfify(n)
+		dgl.rdfs = append(dgl.rdfs, rdfs...)
+		uids = append(uids, uid)
+	}
+
+	return uids, nil
+}
+
+func (dgl *dglite) Read(qs []gql.GraphQuery, in interface{}) error {
+	resolved := dgl.reader.resolveVariables(qs, dgl.rdfs)
+	var nodes []map[string]interface{}
+
+	for _, e := range resolved {
+		if e.Alias == "var" {
+			continue
+		}
+
+		next := dgl.reader.read(e, dgl.rdfs)
+		nodes = append(next, nodes...)
+	}
+
+	js, err := json.Marshal(nodes)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(js, in)
+}
---
+++ dglite_test.go
@@ -0,0 +1,119 @@
+package dglite
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"mooncamp.com/dgx/gql"
+)
+
+type User struct {
+	ID     uint64  `json:"uid"`
+	Name   string  `json:"user.name"`
+	Office *Office `json:"user.office"`
+	Teams  []Team  `json:"user.team"`
+}
+
+type Office struct {
+	ID       uint64    `json:"uid"`
+	Name     string    `json:"office.name"`
+	Location *Location `json:"office.location"`
+}
+
+type Team struct {
+	ID   uint64 `json:"uid"`
+	Name string `json:"team.name"`
+}
+
+type Location struct {
+	City    string `json:"location.city"`
+	Country string `json:"location.country"`
+}
+
+func Test_it_finds_all_locations(t *testing.T) {
+	in := []User{
+		{
+			Name: "francesc",
+			Office: &Office{
+				Name: "berlin office",
+				Location: &Location{
+					City:    "Berlin",
+					Country: "Germany",
+				},
+			},
+		},
+		{
+			Name: "manish",
+			Office: &Office{
+				Name: "amsterdam office",
+				Location: &Location{
+					City:    "Amsterdam",
+					Country: "Netherlands",
+				},
+			},
+		},
+	}
+
+	schema := []Schema{
+		{Predicate: "user.name", Type: "string", Many: false},
+		{Predicate: "user.office", Type: "uid", Many: false},
+		{Predicate: "user.teams", Type: "uid", Many: true},
+		{Predicate: "user.name", Type: "string", Many: false},
+		{Predicate: "office.name", Type: "string", Many: false},
+		{Predicate: "office.location", Type: "uid", Many: false},
+		{Predicate: "location.city", Type: "string", Many: false},
+		{Predicate: "location.country", Type: "string", Many: false},
+		{Predicate: "team.name", Type: "string", Many: false},
+	}
+
+	dgl := New(schema)
+	uids, err := dgl.Write(in)
+	require.NoError(t, err)
+
+	require.Len(t, uids, 2)
+
+	q := []gql.GraphQuery{
+		{
+			Alias: "var",
+			UID:   uids,
+			Func:  &gql.Function{Name: "uid"},
+			Children: []gql.GraphQuery{
+				{
+					Attr: "user.office",
+					Children: []gql.GraphQuery{
+						{
+							Attr: "office.location",
+							Children: []gql.GraphQuery{
+								{Attr: "uid", Var: "office_locations"},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			NeedsVar: []gql.VarContext{{Name: "office_locations"}},
+			Func:     &gql.Function{Name: "uid"},
+			Children: []gql.GraphQuery{
+				{Attr: "location.city"}, {Attr: "location.country"},
+			},
+		},
+	}
+
+	var actual []Location
+	err = dgl.Read(q, &actual)
+	require.NoError(t, err)
+
+	expected := []Location{
+		{
+			City:    "Berlin",
+			Country: "Germany",
+		},
+		{
+			City:    "Amsterdam",
+			Country: "Netherlands",
+		},
+	}
+
+	require.ElementsMatch(t, expected, actual)
+}
